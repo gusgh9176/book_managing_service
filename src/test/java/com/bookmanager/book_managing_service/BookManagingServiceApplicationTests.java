235d14dd2417bfcfa36c228ac9f8197091c9998f
HyunHoMoon <44826442+gusgh9176@users.noreply.github.com>
2020-05-21 17:38:53 +0900

    Initial commit

---
+++ src/test/java/com/bookmanager/book_managing_service/BookManagingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmanager.book_managing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
